c58de1e510144669fee6c2d9210bda974dde89dd
ColorsWind <1748591300@qq.com>
2020-03-26 17:18:27 +0800

    修改启动脚本

--- start.cmd
+++ start.cmd
@@ -1,2 +1,2 @@
-java -server -Xmx128M -Xms128M -XX:+AggressiveOpts -XX:+UseCompressedOops -Dfile.encoding=utf8 -jar SimpleHttp-maven-version.jar
+java -server -Xmx128M -Xms128M -XX:+AggressiveOpts -XX:+UseCompressedOops -Dfile.encoding=utf8 -jar SimpleHttp-0.0.1-SNAPSHOT.jar
 pause
